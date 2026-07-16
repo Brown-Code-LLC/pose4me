acab6e7e29b3d96a651f728a7c4f6f983058ee19
Kojo-Brown <100282824+Kojo-Brown@users.noreply.github.com>
2026-07-15 23:01:48 -0400

    Initial Commit

---
+++ pose-for-me/pose_for_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  pose_for_meApp.swift
+//  pose-for-me
+//
+//  Created by Nicholas Darko Brown on 7/15/26.
+//
+
+import SwiftUI
+
+@main
+struct pose_for_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
